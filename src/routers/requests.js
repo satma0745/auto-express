3d32fe9f8ca14a1c7587ecd472aa85b2fdb40ee2
catman0745 <catman0745@gmail.com>
2021-07-18 23:13:58 +0300

    Add request review

--- src/routers/requests.js
+++ src/routers/requests.js
@@ -1,13 +1,28 @@
-import express from 'express'
+import express, { request } from 'express'
+
+const generateRequestId = (() => {
+  let nextId = 1
+  return () => {
+    const id = nextId
+    nextId += 1
+    return id
+  }
+})()
 
 const requests = []
 
 const router = express.Router()
 
-router.post('/', (req, res) => {
+router.get('/all', (_, res) => {
+  res.status(200).send(requests)
+})
+
+router.post('/submit', (req, res) => {
   const request = {
+    id: generateRequestId(),
     fullName: req.body?.fullName,
     phoneNumber: req.body?.phoneNumber,
+    status: 'pending',
   }
 
   if (!request.fullName) {
@@ -24,8 +39,22 @@ router.post('/', (req, res) => {
   res.sendStatus(200)
 })
 
-router.get('/', (_, res) => {
-  res.status(200).send(requests)
+router.post('/:requestId/review', (req, res) => {
+  const requestId = Number(req.params.requestId)
+  if (!requests.some((request) => request.id === requestId)) {
+    res.sendStatus(404)
+    return
+  }
+
+  const request = requests.find((request) => request.id === requestId)
+  if (request.status !== 'pending') {
+    res.status(403).send('Request has already been reviewed.')
+  }
+
+  request.note = req.body?.note
+  request.status = 'reviewed'
+
+  res.sendStatus(200)
 })
 
 export default router
