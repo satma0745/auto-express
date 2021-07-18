28ba83e955cdf4909406f1746a8ed088a9627e73
catman0745 <catman0745@gmail.com>
2021-07-18 22:22:33 +0300

    Add requests

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 import express from 'express'
 
-const app = express()
+const requests = []
 
-app.get('/', (_, res) => {
-  res.send('Up and running!')
+const app = express()
+app.use(express.json())
+
+app.post('/api/requests', (req, res) => {
+  const request = {
+    fullName: req.body?.fullName,
+    phoneNumber: req.body?.phoneNumber,
+  }
+
+  if (!request.fullName) {
+    res.status(400).send('Specify full name.')
+    return
+  }
+
+  if (!request.phoneNumber) {
+    res.status(400).send('Specify phone number.')
+    return
+  }
+
+  requests.push(request)
+  res.send(200)
+})
+
+app.get('/api/requests', (_, res) => {
+  res.status(200).send(requests)
 })
 
 const port = 5050
